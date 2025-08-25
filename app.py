37c06c8ed90a47dacee8f14efb5ca4851882cad2
sailaxmi-hub <23jr1a05f0@gmail.com>
2025-08-25 19:32:20 +0530

    updating solutions for app

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import pickle
 generateAI()
 ai=pickle.load(open('model.pkl','rb'))
 
-
-app=Flask(__name__)
+app=Flask(_name_)
 
 @app.route('/')
 def home():
@@ -21,5 +20,5 @@ def predict():
     result=result[0]
     return (result)
 
-if(__name__=="__main__"):
+if(_name=="main_"):
     app.run(host='0.0.0.0',port=5000,debug=True)
